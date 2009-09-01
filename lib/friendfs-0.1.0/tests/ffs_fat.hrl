2f722381dc1bf5305f5fbd025826eb869991ff4c
Lukas Larsson <garazdawi@gmail.com>
2009-09-01 10:20:21 +0200

    Emacs tabbed

--- lib/friendfs-0.1.0/tests/ffs_fat.hrl
+++ lib/friendfs-0.1.0/tests/ffs_fat.hrl
@@ -7,33 +7,33 @@
 %%%-------------------------------------------------------------------
 
 init_test_() ->
-	Setup = fun() ->
-				catch ets:delete(?COUNTER_TABLE),
-				init_counters(),
-				Tid = init(test),
-				Tid
-			end,
-	Tests = fun(Tid) ->
-					inode_check(Tid,#ffs_inode{ inode = 1, hash = 0, size = 0, uid = 0,
-											    gid = 0, mode = ?A bor ?D, refcount = 1 })
-			end,
-	{setup,Setup,Tests}.
-	
-	
-	
-	
-	
+    Setup = fun() ->
+		    catch ets:delete(?COUNTER_TABLE),
+		    init_counters(),
+		    Tid = init(test),
+		    Tid
+	    end,
+    Tests = fun(Tid) ->
+		    inode_check(Tid,#ffs_inode{ inode = 1, hash = 0, size = 0, uid = 0,
+						gid = 0, mode = ?A bor ?D, refcount = 1 })
+	    end,
+    {setup,Setup,Tests}.
+
+
+
+
+
 inode_check(Tid,#ffs_inode{ inode = Inode, hash = Hash, size = Size, uid = Uid,
-						    gid = Gid, mode = Mode, refcount = Refs }) ->
-	[
-		?_assertEqual((lookup(Tid,Inode))#ffs_inode.inode,Inode),
-		?_assertEqual((lookup(Tid,Inode))#ffs_inode.hash,Hash),
-		?_assertEqual((lookup(Tid,Inode))#ffs_inode.size,Size),
-		?_assertEqual((lookup(Tid,Inode))#ffs_inode.uid,Uid),
-		?_assertEqual((lookup(Tid,Inode))#ffs_inode.gid,Gid),
-		?_assertEqual((lookup(Tid,Inode))#ffs_inode.mode,Mode),
-		?_assertEqual((lookup(Tid,Inode))#ffs_inode.ctime,(lookup(Tid,1))#ffs_inode.atime),
-		?_assertEqual((lookup(Tid,Inode))#ffs_inode.atime,(lookup(Tid,1))#ffs_inode.mtime),
-		?_assertEqual((lookup(Tid,Inode))#ffs_inode.refcount,Refs)
-	].
+			    gid = Gid, mode = Mode, refcount = Refs }) ->
+    [
+     ?_assertEqual((lookup(Tid,Inode))#ffs_inode.inode,Inode),
+     ?_assertEqual((lookup(Tid,Inode))#ffs_inode.hash,Hash),
+     ?_assertEqual((lookup(Tid,Inode))#ffs_inode.size,Size),
+     ?_assertEqual((lookup(Tid,Inode))#ffs_inode.uid,Uid),
+     ?_assertEqual((lookup(Tid,Inode))#ffs_inode.gid,Gid),
+     ?_assertEqual((lookup(Tid,Inode))#ffs_inode.mode,Mode),
+     ?_assertEqual((lookup(Tid,Inode))#ffs_inode.ctime,(lookup(Tid,1))#ffs_inode.atime),
+     ?_assertEqual((lookup(Tid,Inode))#ffs_inode.atime,(lookup(Tid,1))#ffs_inode.mtime),
+     ?_assertEqual((lookup(Tid,Inode))#ffs_inode.refcount,Refs)
+    ].
 	
