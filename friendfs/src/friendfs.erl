023770bd467d37fbcbcad06ea48ae446356f0b14
Petter Sandholdt <petterl@lysator.liu.se>
2009-09-21 15:01:21 +0200

    Updated documentation for application file

--- friendfs/src/friendfs.erl
+++ friendfs/src/friendfs.erl
@@ -1,26 +1,57 @@
+%%%-------------------------------------------------------------------
+%%% @author Petter Sandholdt <petter@sandholdt.se>
+%%% @author Lukas Larsson <garazdawi@gmail.com>
+%%% @copyright 2009 Petter Sandholdt & Lukas Larsson
+%%%
+%%% @doc
+%%% Main Application file
+%%%
+%%% Starts the application and the fuserl application
+%%% Fetches the configuration and reads it and starts the supervisors.
+%%%
+%%% @end
+%%%
+%%% Created : 10 Aug 2009 by Petter Sandholdt <petter@sandholdt.se>
+%%%-------------------------------------------------------------------
+
 -module (friendfs).
 -behaviour (application).
 -export ([ start/0,
            start/2,
            stop/0,
            stop/1,
-	   add_config/2]).
+           add_config/2]).
 
 %-=====================================================================-
 %-                        application callbacks                        -
 %-=====================================================================-
 
-%% @hidden
+%%--------------------------------------------------------------------
+%% @doc
+%% Start friendfs application
+%%
+%% @spec
+%%   start() -> ok
+%% @end
+%%--------------------------------------------------------------------
 
 start() ->
     start(permanent).
 
+%hidden
 start(Type) ->
     application:start(fuserl),
     application:start(friendfs, Type).
 
-%% @hidden
-
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Fetch config and start supervisors
+%%
+%% @spec
+%%   start(_Type, _Args) -> ok
+%% @end
+%%--------------------------------------------------------------------
 start(_Type, _Args) ->
     {ok,ConfigPath} = application:get_env(friendfs,config_path),
     {ok,DefaultsPath} = application:get_env(friendfs,config_default_path),
@@ -36,9 +67,17 @@ start(_Type, _Args) ->
     
     friendfs_sup:start_link(Config).
 
-%% @hidden
-
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%%   Stop friendfs and fuserl application
+%%
+%% @spec
+%%   stop() -> ok
+%% @end
+%%--------------------------------------------------------------------
 stop () ->
+  application:stop(fuserl),
   application:stop(friendfs).
 
 %% @hidden
@@ -46,6 +85,16 @@ stop () ->
 stop(_State) ->
   ok.
 
+%%--------------------------------------------------------------------
+%% @doc
+%%   Mount a filesystem
+%%
+%% @spec
+%%   add_config(MountPoint, Opts) -> ok
+%%     MountPoint = string()
+%%     Opts = list()
+%% @end
+%%--------------------------------------------------------------------
 add_config(MountPoint,Opts) ->
     { ok, LinkedIn } = application:get_env(friendfs, linked_in),
     { ok, MountOpts } = application:get_env (friendfs, mount_opts),
