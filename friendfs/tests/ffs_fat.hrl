11fd8596d16ef2ed3114db1d070417abca0380c3
Lukas Larsson <garazdawi@gmail.com>
2009-10-15 17:04:18 +0200

    Added write cache test

--- friendfs/tests/ffs_fat.hrl
+++ friendfs/tests/ffs_fat.hrl
@@ -13,9 +13,15 @@ init_test_() ->
 		    Tid = init(test,1 bsl 15),
 		    Tid
 	    end,
-    Tests = fun(Tid) ->
-		    inode_check(Tid,#ffs_inode{ inode = 1, hash = 0, size = 0, uid = 0,
-						gid = 0, mode = 8#755 bor ?D, refcount = 1 })
+    Tests =
+	fun(Tid) ->
+		inode_check(Tid,#ffs_inode{ inode = 1,
+					    hash = 0,
+					    size = 0,
+					    uid = -1,
+					    gid = -1,
+					    mode = 8#755 bor ?D,
+					    refcount = 1 })
 	    end,
     {setup,Setup,Tests}.
 
@@ -37,3 +43,26 @@ inode_check(Tid,#ffs_inode{ inode = Inode, hash = Hash, size = Size, uid = Uid,
      ?_assertEqual((lookup(Tid,Inode))#ffs_inode.refcount,Refs)
     ].
 	
+
+
+write_test_() ->
+    Setup = fun() ->
+		    catch ets:delete(?COUNTER_TABLE),
+		    init_counters(),
+		    application:start(crypto),
+		    Tid = init(test,1 bsl 10),
+		    Tid
+	    end,
+    Tests = fun(Tid) ->
+		    NewInode = create(Tid,1,"test.txt",-1,-1,8#755,0,0),
+		    Chunks1 = write_cache(Tid,NewInode#ffs_inode.inode,
+					  <<"test text">>,0),
+		    Chunks2 = flush_cache(Tid,NewInode#ffs_inode.inode),
+		    [?_assertEqual([],Chunks1),
+		     ?_assertEqual(
+			{chunk,"6AFC05EAE22E994F1C7DD48E58F8895DD9028223",
+			 <<"test text">>},Chunks2)]
+	    end,
+    {setup,Setup,Tests}.
+
+
